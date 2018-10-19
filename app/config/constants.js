ac4ad701aa08cc6259d797fe8e848391c0ae4295
Ger <german.depina@mercadolibre.com>
2018-10-19 20:35:31 -0300

    para mati

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,7 +1,10 @@
-export const FIREBASE_API_KEY = "<YOUR_FIREBASE_API_KEY>";
-export const FIREBASE_AUTH_DOMAIN = "<YOUR_FIREBASE_AUTH_DOMAIN>";
-export const FIREBASE_DATABASE_URL = "<YOUR_FIREBASE_DATABASE_URL>";
-export const FIREBASE_PROJECT_ID = "<YOUR_FIREBASE_PROJECT_ID>";
-export const FIREBASE_STORAGE_BUCKET = "<YOUR_FIREBASE_STORAGE_BUCKET>";
-export const FIREBASE_MESSAGING_SENDER_ID = "<YOUR_FIREBASE_MESSAGING_SENDER_ID>";
-export const FACEBOOK_APP_ID = "<YOUR_FACEBOOK_APP_ID>";
+export const APP_NAME = 'DriveShoot';
+
+export const FIREBASE_API_KEY = "AIzaSyAaLoN2ypRkf3ya836Ws1jfffgAQ8cErRc";
+export const FIREBASE_AUTH_DOMAIN = "driveshoot-e5300.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://driveshoot-e5300.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "driveshoot-e5300";
+export const FIREBASE_STORAGE_BUCKET = "driveshoot-e5300.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "11686507066";
+
+export const FACEBOOK_APP_ID = '1130450777103073';
